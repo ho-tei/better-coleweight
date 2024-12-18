0043501e4a943eccdd95f69ac89c2af54f7b60d4
vanguard <117127416+ho-tei@users.noreply.github.com>
2024-12-18 10:29:58 +0100

    Attempt to fix broken time to find scrap

--- chat/scrapEgo.js
+++ chat/scrapEgo.js
@@ -1,13 +1,18 @@
-import constants from "../util/constants"
+import constants from "../util/constants";
 
 let enterTime = undefined;
 register("chat", (name) => {
-    if(name != Player.getName())
-        return;
-    enterTime = Date.now()
-}).setChatCriteria(/ ⛏ (.*) entered the mineshaft!/)
-
+  if (name != Player.getName()) {
+    enterTime = undefined;
+  } else {
+    enterTime = Date.now();
+  }
+}).setChatCriteria(/(.*) entered the mineshaft!/);
 
 register("chat", () => {
-    ChatLib.chat(`${constants.PREFIX}&bYou found a scrap in ${(Date.now()-enterTime)/1000} seconds!`)
-}).setChatCriteria("EXCAVATOR! You found a Suspicious Scrap!")
+  ChatLib.chat(
+    `${constants.PREFIX}&bYou found a scrap in ${
+      (Date.now() - enterTime) / 1000
+    } seconds!`
+  );
+}).setChatCriteria("EXCAVATOR! You found a Suspicious Scrap!");
